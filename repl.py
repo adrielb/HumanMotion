01798b1a5330bdf66e2602e6bb2bd3ca65348c68
Adriel Bergmann <adrielb@users.noreply.github.com>
2015-01-27 19:38:57 -0500

    trying SLIC superpixels

--- repl.py
+++ repl.py
@@ -47,3 +47,59 @@ tidy = np.asarray([ (v, u, img[v,u,0], img[v,u,1], img[v,u,2] )
 tidy.shape
 
 np.savetxt( '/tmp/img.csv', tidy,  delimiter=',', fmt='%d' )
+
+
+### SLIC
+import matplotlib.pyplot as plt
+import numpy as np
+
+from skimage import data
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
+from skimage.util import img_as_float
+from skimage.viewer import ImageViewer
+from skimage.color import label2rgb
+
+viewer = ImageViewer( data.coffee() )
+viewer.show()
+
+img = img_as_float( data.coffee()[::2,::2] )
+
+from scipy import misc
+img = misc.imread('/home/abergman/walking/img/output-00125.png')  
+
+n_segs=np.prod( np.array(img.shape)[0:2] / 10 )
+
+import matplotlib as mpl
+mpl.rcParams['image.interpolation'] = 'nearest'
+
+segments = slic( img, n_segments=n_segs, compactness=5, sigma=2)
+print  "SLIC # of segments: %d " % len(np.unique( segments )) 
+overlay = label2rgb( segments, image=img)
+plt.close()
+plt.imshow( overlay )
+plt.show(block=False)
+
+plt.close()
+plt.imshow( segments, cmap="prism" )
+plt.show(block=False)
+
+plt.close()
+plt.imshow( mark_boundaries( img, segments) )
+plt.show(block=False)
+
+segments = slic( img, n_segments=n_segs, slic_zero=True )
+print  "SLIC # of segments: %d " % len(np.unique( segments )) 
+overlay = label2rgb( segments, image=img)
+plt.close()
+plt.imshow( overlay )
+plt.show(block=False)
+
+segments.shape
+
+np.save('/tmp/a-0001.npy',segments)
+
+np.savez('/tmp/a-0002.npz',segments=segments)
+
+np.load( '/tmp/a-0002.npz' )['segments']
+
